f1b9dee837047cf3b40bb15520ceccdaaef4023c
jensenhuangyankai <jensen@huangyankai.com>
2024-06-30 18:08:34 +0800

    attempt 3.1

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>You're not supposed to be here!</h1>
+      <h1>You&apos;re not supposed to be here!</h1>
     </main>
   );
 }
