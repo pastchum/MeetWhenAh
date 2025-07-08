aceb7cf39c639b046cbf8beaf7fb33f81724ddf1
pastchum <patrick.navin.thomas123@gmail.com>
2025-07-08 16:44:27 +0800

    Fix import issue

--- bot/telegram/handlers/command_handlers.py
+++ bot/telegram/handlers/command_handlers.py
@@ -7,7 +7,6 @@ from tzlocal import get_localzone
 from ..config.config import bot
 from ..services.database_service import getEntry, setEntry, updateEntry
 from ..services.user_service import updateUsername, setUserSleepPreferences
-from ..services.availability_service import getUserAvailability
 from ..utils.message_templates import WELCOME_MESSAGE, HELP_MESSAGE
 from ..utils.web_app import create_web_app_url
 from urllib.parse import urlencode
